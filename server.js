46c9cc32d354b7d34844298006f6a0e735bc856e
HeviDevi <devon.r.ross@gmail.com>
2024-06-19 13:08:32 -0400

    Added handlebars config and basic file structure for handlebars, added empty model files to be filled later

--- server.js
+++ server.js
@@ -1,15 +1,39 @@
-const express = require('express');
+const express = require('express')
+const session = require('express-session')
+require('dotenv').config()
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
+const { exphbs, engine } = require('express-handlebars')
 // const routes = require('./routes');
-const sequelize = require('./config/connection');
-const app = express();
-const PORT = process.env.PORT || 5432;
+const sequelize = require('./config/connection')
+const app = express()
+const PORT = process.env.PORT || 5432
 
-// app.use(express.json());
-// app.use(express.urlencoded({extended : true}))
+
+
+const sessionConfig = {
+    secret: process.env.DB_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    cookie: {
+        httpOnly: true,
+    },
+  }
+
+
+app.engine('handlebars', engine({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+app.use(express.json());
+app.use(express.urlencoded({extended : true}))
 // app.use(routes);
+app.use(session(sessionConfig))
+
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => {
         console.log(`App listening on port ${PORT}!`)
     })
-});
+})
